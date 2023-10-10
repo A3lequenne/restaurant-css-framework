9289fec95517fa1f55c13212e7918c07cdf828d9
Clément Lequenne <clementlequenne1@gmail.com>
2023-10-10 17:44:36 +0200

    will finish tonight

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,9 @@ export default {
         menu: resolve(root, 'menu', 'index.html'),
         about_us: resolve(root, 'about_us', 'index.html'),
         gallery: resolve(root, 'gallery', 'index.html'),
-        contact: resolve(root, 'contact', 'index.html')
+        contact: resolve(root, 'contact', 'index.html'),
+        gallery_2: resolve(root, 'gallery_2', 'index.html'),
+        gallery_3: resolve(root, 'gallery_3', 'index.html'),
       }
     }
   }
